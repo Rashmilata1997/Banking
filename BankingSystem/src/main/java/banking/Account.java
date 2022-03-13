97b1ab05caf1b3d8de6b08429711781a84336439
Rashmilata1997 <101513323+Rashmilata1997@users.noreply.github.com>
2022-03-13 20:13:39 +0530

    Update Account.java

--- BankingSystem/src/main/java/banking/Account.java
+++ BankingSystem/src/main/java/banking/Account.java
@@ -17,7 +17,7 @@ public abstract class Account {
 	private double balance;
 
 	protected Account(AccountHolder accountHolder, Long accountNumber, int pin, double startingDeposit) {
-		// complete the constructor
+		this.accountHolder=accountHolder;
 	}
 
 	public AccountHolder getAccountHolder() {
